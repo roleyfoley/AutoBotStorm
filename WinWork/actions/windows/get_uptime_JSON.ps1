5759fbafa568ea9167ff7266936f22b67fd073a8
roleyfoley <roleyfoley@gmail.com>
2017-05-01 23:07:44 +1000

    Added WinWork Folder

---
+++ WinWork/actions/windows/get_uptime_JSON.ps1
@@ -0,0 +1,15 @@
+function Get-Uptime {
+   $ReturnObject = New-OBject Psobject
+   $os = Get-WmiObject win32_operatingsystem
+   $uptime = (Get-Date) - ($os.ConvertToDateTime($os.lastbootuptime))
+   $CurrentTime = Get-Date
+	$REturnObject | Add-Member -MemberType NoteProperty -Name CurrentTime -Value "$($CurrentTime)"
+	$ReturnObject | Add-Member -MemberType NoteProperty -Name OSLastBooTime -Value "$($os.ConvertToDateTime($os.lastbootuptime))"
+	$ReturnObject | ADd-member -MemberType NOteProperty -Name Uptime -Value "Uptime: $( $Uptime.Days) days, $($Uptime.Hours) hours, $($Uptime.Minutes) minutes"
+
+	$ReturnObject = Convertto-Json $ReturnObject
+	return $ReturnObject
+}
+
+Clear-Host
+Get-Uptime
